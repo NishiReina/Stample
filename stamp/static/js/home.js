0ad52aee7aad16978ec8fdc184082902d5a8c897
NishiReina <s1f101901195@iniad.org>
2022-01-10 20:37:01 +0900

    iroiro

---
+++ stamp/static/js/home.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+
+    const stampBtn = document.getElementById("stamp");
+    
+}
+
+const openBtn = document.getElementById('modal-open');
+const closeBtn = document.getElementById('modal-close');
+const modal = document.getElementById('modal');
+
+openBtn.addEventListener('click', () => {
+    modal.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+})
