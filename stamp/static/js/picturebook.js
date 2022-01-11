627803209fc1ad6375253a5fbf97b8ab0b4050e8
NishiReina <s1f101901195@iniad.org>
2022-01-11 21:31:57 +0900

    80perkurai

--- stamp/static/js/picturebook.js
+++ stamp/static/js/picturebook.js
@@ -34,6 +34,7 @@
 
 // };
 
+// スタンプモーダル
 const stampOpenBtns = document.getElementsByClassName('picturebook__modal-open');
 const stampCloseBtns = document.getElementsByClassName('picturebook__modal-close');
 const stampModal = document.getElementsByClassName('picturebook__modal');
@@ -43,6 +44,12 @@ const picturebook = document.getElementsByClassName('picturebook__stamps');
 const stamps = document.getElementsByClassName('picturebook__stamp');
 const btn = document.getElementsByClassName('picturebook__btn');
 
+// detailモーダル
+const detailModal = document.getElementsByClassName('picturebook__modal-detail');
+const detailOpenBtns = document.getElementsByClassName('picturebook__todetail-btn');
+const detailCloseBtns = document.getElementsByClassName('modal__detail-close');
+
+// stamp modal
 console.log(stampOpenBtns);
 console.log(stampModal);
 for (let i = 0; i < stampOpenBtns.length; i++) {
@@ -72,6 +79,39 @@ function stampOpen() {
 }
 
 function stampClose() {
+    stampModal[this.value].style.display = 'none';
+    bg[0].style.background = 'none';
+    title[0].style.display = 'flex';
+    btn[0].style.display = 'block';
+    picturebook[0].classList.remove('modal-block');
+    for (let i = 0; i < stamps.length; i++){
+        stamps[i].style.display = 'block';
+    }
+}
+
+
+// detail modal
+for (let i = 0; i < detailOpenBtns.length; i++) {
+    console.log(detailOpenBtns[i]);
+    console.log(detailModal[i]);
+    detailOpenBtns[i].addEventListener('click', {
+        value: i,
+        handleEvent: detailOpen
+    });
+
+    detailCloseBtns[i].addEventListener('click', {
+        value: i,
+        handleEvent: detailClose
+    });
+}
+
+function detailOpen() {
+    detailModal[this.value].style.display = 'block';
+    stampModal[this.value].style.display = 'none';
+}
+
+function detailClose() {
+    detailModal[this.value].style.display = 'none';
     stampModal[this.value].style.display = 'none';
     bg[0].style.background = 'none';
     title[0].style.display = 'flex';
